7956e9498caf9078b53ae1df4c9522676431612b
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-23 11:49:34 -0300

    Add put

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ const livros = [
     }
 ]
 
+function buscaLivroPorId(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })};
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
@@ -22,9 +27,20 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivroPorId(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro adicionado com sucesso');
 });
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivroPorId(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros); 
+})
 
 export default app;
