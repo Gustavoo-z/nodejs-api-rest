6676a3bc7f43e934caa9fba9ba7338d5894298cb
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-24 11:20:19 -0300

    Add mongoose and connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import conectaDB from './config/dbConnect.js';
+
+const conexao = await conectaDB();
+
+conexao.on('error', (erro) => {
+    console.error('Erro ao conectar ao MongoDB: ', erro);
+});
+
+conexao.once('open', () => {
+    console.log('Conectado ao MongoDB');
+});
 
 const app = express();
 app.use(express.json());
