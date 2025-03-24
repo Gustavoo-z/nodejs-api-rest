410403af3b8d1a7fe5ec9db89dcfa41dfaafd420
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-24 14:54:02 -0300

    Adjusting get in mongodb

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import conectaDB from './config/dbConnect.js';
+import livro from './models/Livro.js';
 
 const conexao = await conectaDB();
 
@@ -14,29 +15,19 @@ conexao.once('open', () => {
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: 'O Senhor dos Anéis'
-    },
-    {
-        id: 2,
-        titulo: 'O Hobbit'
-    }
-]
-
-function buscaLivroPorId(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })};
-
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
 
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
-});
+app.get('/livros', async (req, res) => {
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    }
+  });  
 
 app.get('/livros/:id', (req, res) => {
     const index = buscaLivroPorId(req.params.id);
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,7 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 async function conectaDB() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.emzrt.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0")
-
+    mongoose.connect(process.env.DB_CONNECTION_STRING)
     return mongoose.connection;
 };
 
