16aeef2b7f69b7b35dbd02e33a92cea8533b09fc
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-25 15:09:47 -0300

    Add controller post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import conectaDB from './config/dbConnect.js';
-import livro from './models/Livro.js';
+import routes from './routes/index.js';
 
 const conexao = await conectaDB();
 
@@ -13,21 +13,12 @@ conexao.once('open', () => {
 });
 
 const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node.js');
-});
+routes(app);
 
 app.get('/livros/:id', (req, res) => {
     const index = buscaLivroPorId(req.params.id);
     res.status(200).json(livros[index]);
 })
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro adicionado com sucesso');
-});
 
 app.put('/livros/:id', (req, res) => {
     const index = buscaLivroPorId(req.params.id);
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -12,6 +12,15 @@
           }
     }
 
+    static async cadastrarLivro (req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            res.status(201).json({ message: 'Livro cadastrado com sucesso', livro: novoLivro });
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` });
+        }
+    }
+    
  }
 
  export default LivroController;
