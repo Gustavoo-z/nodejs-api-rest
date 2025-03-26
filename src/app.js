56f3ea1927d3ba0a6a461df75e10525bf0d950ac
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-03-26 10:59:58 -0300

    Add controller put and  delete

--- src/app.js
+++ src/app.js
@@ -15,17 +15,6 @@ conexao.once('open', () => {
 const app = express();
 routes(app);
 
-app.get('/livros/:id', (req, res) => {
-    const index = buscaLivroPorId(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
-app.put('/livros/:id', (req, res) => {
-    const index = buscaLivroPorId(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros); 
-})
-
 app.delete('/livros/:id', (req, res) => {
     const index = buscaLivroPorId(req.params.id);
     livros.splice(index, 1);
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -8,7 +8,18 @@
             res.status(200).json(listaLivros);
           } catch (erro) {
             console.error(erro);
-            res.status(500).json({ error: erro.message });
+            res.status(500).json({ message: `${erro.message} - falha na requisição` });
+          }
+    }
+
+    static async listarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+          } catch (erro) {
+            console.error(erro);
+            res.status(500).json({ message: `${erro.message} - falha na busca do livro` });
           }
     }
 
@@ -21,6 +32,27 @@
         }
     }
     
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: 'Livro atualizado com sucesso'});
+          } catch (erro) {
+            console.error(erro);
+            res.status(500).json({ message: `${erro.message} - falha na atualização do livro` });
+          }
+    }
+
+    static async removerLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({message: 'Livro removido com sucesso'});
+          } catch (erro) {
+            console.error(erro);
+            res.status(500).json({ message: `${erro.message} - falha ao remover livro` });
+          }
+    }
  }
 
  export default LivroController;
